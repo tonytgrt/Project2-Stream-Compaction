b513ba2d1c6b6f7889329b74e8bf4fda12f32ee3
tonytgrt <tonytg@seas.upenn.edu>
2025-09-10 21:38:19 -0400

    part 2

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -12,6 +12,47 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
+        __global__ void kernNaiveScan(int n, int lim, int *odata, int *idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index >= n) {
+                return;
+            }
+
+            for (int d = 1; d <= lim; d++) {
+                if (index >= (1 << (d - 1))) {
+                    odata[index] = idata[index - (1 << (d - 1))] + idata[index];
+
+                }
+                else {
+                    odata[index] = idata[index];
+                }
+                __syncthreads();
+                idata[index] = odata[index];
+            }
+
+			odata[index] = (index == 0) ? 0 : idata[index - 1];
+            
+		}
+
+        __global__ void kernNaiveScanIteration(int n, int d, int* odata, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index >= n) return;
+
+            if (index >= (1 << (d - 1))) {
+                odata[index] = idata[index - (1 << (d - 1))] + idata[index];
+            }
+            else {
+                odata[index] = idata[index];
+            }
+        }
+
+        __global__ void kernNaiveScanShift(int n, int* odata, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index >= n) return;
+            odata[index] = (index == 0) ? 0 : idata[index - 1];
+		}
+
+
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
@@ -19,6 +60,33 @@ namespace StreamCompaction {
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
+            int* d_idata;
+			int* d_odata;
+			cudaMalloc((void**)&d_idata, n * sizeof(int));
+			cudaMalloc((void**)&d_odata, n * sizeof(int));
+			cudaMemcpy(d_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+			cudaDeviceSynchronize();
+
+			int blockSize = 1024;
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+            if (n <= blockSize) {
+                kernNaiveScan << <fullBlocksPerGrid, blockSize >> > (n, ilog2ceil(n), d_odata, d_idata);
+            } else {
+                for (int d = 1; d <= ilog2ceil(n); d++) {
+                    kernNaiveScanIteration << <fullBlocksPerGrid, blockSize >> > (n, d, d_odata, d_idata);
+                    std::swap(d_idata, d_odata); // Swap pointers
+                }
+                kernNaiveScanShift << <fullBlocksPerGrid, blockSize >> > (n, d_odata, d_idata);
+            }
+
+            
+            
+			cudaDeviceSynchronize();
+			cudaMemcpy(odata, d_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+			cudaFree(d_idata);
+			cudaFree(d_odata);
+
             timer().endGpuTimer();
         }
     }

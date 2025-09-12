46d0d8aabff159b7493b6709a4c5747359a93c11
tonytgrt <tonytg@seas.upenn.edu>
2025-09-12 14:48:58 -0400

    compact

--- stream_compaction/common.cu
+++ stream_compaction/common.cu
@@ -24,6 +24,9 @@ namespace StreamCompaction {
          */
         __global__ void kernMapToBoolean(int n, int *bools, const int *idata) {
             // TODO
+			int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (index >= n) return;
+			bools[index] = (idata[index] != 0) ? 1 : 0;
         }
 
         /**
@@ -33,6 +36,11 @@ namespace StreamCompaction {
         __global__ void kernScatter(int n, int *odata,
                 const int *idata, const int *bools, const int *indices) {
             // TODO
+			int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (index >= n) return;
+            if (bools[index] == 1) {
+                odata[indices[index]] = idata[index];
+            }
         }
 
     }
--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -100,6 +100,37 @@ namespace StreamCompaction {
             timer().endGpuTimer();
         }
 
+        // Wrapper for compact, removes timer and takes in device pointers
+        void scanCompact(int n, int* odata, const int* idata) {
+            // TODO
+            int paddedSize = nextPowerOf2(n);
+
+            int* d_idata;
+            int* d_odata;
+            cudaMalloc((void**)&d_idata, paddedSize * sizeof(int));
+            cudaMalloc((void**)&d_odata, paddedSize * sizeof(int));
+
+            cudaMemset(d_idata, 0, paddedSize * sizeof(int));
+            cudaMemcpy(d_idata, idata, n * sizeof(int), cudaMemcpyDeviceToDevice);
+            cudaDeviceSynchronize();
+
+            int numThreads = paddedSize / 2;
+
+            if (paddedSize <= 2048) {
+                kernEffScan << <1, numThreads, n * sizeof(int) >> > (d_odata, d_idata, paddedSize);
+            }
+            else {
+
+            }
+
+            cudaMemcpy(odata, d_odata, n * sizeof(int), cudaMemcpyDeviceToDevice);
+            cudaDeviceSynchronize();
+
+            cudaFree(d_idata);
+            cudaFree(d_odata);
+
+        }
+
         /**
          * Performs stream compaction on idata, storing the result into odata.
          * All zeroes are discarded.
@@ -112,8 +143,47 @@ namespace StreamCompaction {
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
+			int* d_idata;
+			int* d_odata;
+
+			int* d_bools;
+			int* d_indices;
+
+			cudaMalloc((void**)&d_idata, n * sizeof(int));
+			cudaMalloc((void**)&d_odata, n * sizeof(int));
+			cudaMalloc((void**)&d_bools, n * sizeof(int));
+			cudaMalloc((void**)&d_indices, n * sizeof(int));
+
+			cudaMemcpy(d_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+			cudaDeviceSynchronize();
+
+			int blockSize = 1024;
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+			StreamCompaction::Common::kernMapToBoolean << <fullBlocksPerGrid, blockSize >> > (n, d_bools, d_idata);
+			cudaDeviceSynchronize();
+
+			scanCompact(n, d_indices, d_bools);
+			cudaDeviceSynchronize();
+
+			StreamCompaction::Common::kernScatter << <fullBlocksPerGrid, blockSize >> > (n, d_odata, d_idata, d_bools, d_indices);
+			cudaDeviceSynchronize();
+
+			int lastBool, lastIndex;
+			cudaMemcpy(&lastBool, d_bools + n - 1, sizeof(int), cudaMemcpyDeviceToHost);
+			cudaMemcpy(&lastIndex, d_indices + n - 1, sizeof(int), cudaMemcpyDeviceToHost);
+
+			int count = lastBool + lastIndex;
+			cudaMemcpy(odata, d_odata, count * sizeof(int), cudaMemcpyDeviceToHost);
+			cudaDeviceSynchronize();
+
+			cudaFree(d_idata);
+			cudaFree(d_odata);
+			cudaFree(d_bools);
+			cudaFree(d_indices);
+
             timer().endGpuTimer();
-            return -1;
+            return count;
         }
     }
 }
